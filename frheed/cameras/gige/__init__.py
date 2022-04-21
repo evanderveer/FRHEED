fa8f2ea501e14408fe806bffe1112ab1805a97a5
Ewout van der Veer <evanderveer@users.noreply.github.com>
2022-04-21 22:41:55 +0200

    Started implementing GigECamera class

--- frheed/cameras/gige/__init__.py
+++ frheed/cameras/gige/__init__.py
@@ -1,97 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Connecting to USB cameras.
+Connecting to GigE Vision cameras.
 """
 
 import os
 from typing import Union, Optional, List, Tuple
 import time
+import vimba
 import cv2
 import numpy as np
 
 from frheed.cameras import CameraError
 
-# Suppress warning from MSMF backend bug
-# https://stackoverflow.com/a/54585850/10342097
-os.environ["OPENCV_VIDEOIO_PRIORITY_MSMF"] = "0"
 
 _DEBUG = (__name__ == "__main__")
 
-# Get non-platform-specific capture properties (0 < id < 50)
-_CAP_PROPS = [prop for prop in dir(cv2) if prop.startswith("CAP_PROP")
-              and getattr(cv2, prop, 1000) < 50]
-
-# Editable camera settings to show
-_GUI_SETTINGS = {
-    "CAP_PROP_BRIGHTNESS":      "Brightness",
-    "CAP_PROP_CONTRAST":        "Contrast",
-    "CAP_PROP_SATURATION":      "Saturation",
-    "CAP_PROP_GAIN":            "Gain",
-    "CAP_PROP_EXPOSURE":        "Exposure",
-    }
-
-# Read-only camera information to show
-_GUI_INFO = {
-    "CAP_PROP_FRAME_WIDTH":     "Frame Width",
-    "CAP_PROP_FRAME_HEIGHT":    "Frame Height",
-    "CAP_PROP_TEMPERATURE":     "Temperature",  # TODO: figure out units
-    
-    }
-
-# Backend for cameras
-_DEFAULT_BACKEND = cv2.CAP_DSHOW  # cv2.CAP_DSHOW or cv2.CAP_MSMF
-
-def list_cameras() -> List[int]:
-    pass
-    """
-    Get list of indices of available USB cameras.
-
-    Returns
-    -------
-    List[int]
-        List of indices of available USB cameras.
-
-    """
-    
-    # Add camera indices until list is exhausted
-    cam_list = []
-    
-    for idx in range(100):
-        cap = cv2.VideoCapture(idx, _DEFAULT_BACKEND)
-        if not cap.read()[0]:
-            break
-        else:
-            cam_list.append(idx)
-        cap.release()
-    
-    # This seems to help fix FPS when using backend CAP_DSHOW
-    cv2.destroyAllWindows()
-        
-    return cam_list
 
 def get_available_cameras() -> dict:
-    pass
-    """ Get available cameras as a dictionary of {source: name}. """
-    cams = list_cameras()
-    available = {}
+    """ Get available GigE cameras as a dictionary of {source: name}. """
+    cam_dict = {}
+    with vimba.Vimba.get_instance() as vim:
+        cams = vim.get_all_cameras()
+        
+        for cam_num, cam_src in enumerate(cams):
+            cam_dict[cam_src] = cam_num
     
-    for src in cams:
-        try:
-            with GigECamera(src=src) as cam:
-                if cam.initialized:
-                    available[src] = str(cam)
-                else:
-                    print(f"USB camera {src} is not available")
-        except CameraError:
-            print("No USB cameras detected")
-            
-    return available
-
-
+    return(available)
+    
 class GigECamera:
-    pass
-    """
-    A class used to encapsulate a cv2.VideoCapture camera.
+    """ 
+    A class used to encapsulate a Vimba GigE camera.
     
     Attributes
     ----------
@@ -103,37 +41,10 @@ class GigECamera:
     camera_methods : dictionary
         Contains all of the camera methods
     
-    """
+    """#### FIX THIS !!!
     
-    _cap_props = {
-        "CAP_PROP_POS_MSEC":         "Current position of the video file in milliseconds.",
-        "CAP_PROP_POS_FRAMES":       "0-based index of the frame to be decoded/captured next.",
-        "CAP_PROP_POS_AVI_RATIO":    "Relative position of the video file: 0 - start, 1 - end.",
-        "CAP_PROP_FRAME_WIDTH":      "Width of the frames in the video stream.",
-        "CAP_PROP_FRAME_HEIGHT":     "Height of the frames in the video stream.",
-        "CAP_PROP_FPS":              "Frame rate.",
-        "CAP_PROP_FOURCC":           "4-character code of codec.",
-        "CAP_PROP_FRAME_COUNT":      "Number of frames in the video file.",
-        "CAP_PROP_FORMAT":           "Format of the Mat objects returned by retrieve().",
-        "CAP_PROP_MODE":             "Backend-specific value indicating the current capture mode.",
-        "CAP_PROP_BRIGHTNESS":       "Brightness of the image (only for cameras).",
-        "CAP_PROP_CONTRAST":         "Contrast of the image (only for cameras).",
-        "CAP_PROP_SATURATION":       "Saturation of the image (only for cameras).",
-        "CAP_PROP_HUE":              "Hue of the image (only for cameras).",
-        "CAP_PROP_GAIN":             "Gain of the image (only for cameras).",
-        "CAP_PROP_EXPOSURE":         "Exposure (only for cameras).",
-        "CAP_PROP_CONVERT_RGB":      "Boolean flags indicating whether images should be converted to RGB.",
-        "CAP_PROP_RECTIFICATION":    "Rectification flag for stereo cameras.",
-        "CAP_PROP_ISO_SPEED":        "The ISO speed of the camera.",
-        "CAP_PROP_BUFFERSIZE":       "Amount of frames stored in internal buffer memory.",
-        }
     
-    def __init__(
-            self, 
-            src: Union[int, str] = 0, 
-            lock: bool = False,
-            backend: Optional[int] = _DEFAULT_BACKEND # cv2.CAP_DSHOW
-            ):
+    def __init__(self, src = 0, lock = False):
         """
         Parameters
         ----------
@@ -150,21 +61,18 @@ class GigECamera:
         super().__setattr__("camera_methods", {})
         super().__setattr__("lock", lock)
         
-        cam_list = list_cameras()
-        if _DEBUG:
-            print(f"Found {len(cam_list)} USB camera(s)")
-        
-        if not cam_list:
-            raise CameraError("No USB cameras detected.")
         
         self._src_type = type(src)
         self._src = src
         
+        self.name = f"GigE{self._src}"
+        self.camera_type = "GigE"
+        
         # Initialize the camera, either by index or filepath (to video)
         if backend is not None:
-            self._cam = cv2.VideoCapture(src, backend)
+            self.cam = cv2.VideoCapture(src, backend)
         else:
-            self._cam = cv2.VideoCapture(src)
+            self.cam = cv2.VideoCapture(src)
             
         # Get camera attributes
         for attr in _CAP_PROPS:
@@ -173,9 +81,9 @@ class GigECamera:
                 self.camera_attributes[attr]["description"] = self._cap_props[attr]
         
         # Other attributes which may be accessed later
-        self._running = True  # camera is running as soon as you connect to it
+        self.running = True  # camera is running as soon as you connect to it
         self._frame_times = []
-        self._incomplete_image_count = 0
+        self.incomplete_image_count = 0
         
     def __getattr__(self, attr: str) -> object:
         # Add this in so @property decorator works as expected
@@ -227,31 +135,12 @@ class GigECamera:
         self.close()
         
     def __str__(self) -> str:
-        return f"USB (Port {self._src})"
-    
-    @property
-    def name(self) -> str:
-        return f"USB{self._src}"
-    
-    @property
-    def camera_type(self) -> str:
-        return "USB"
-    
-    @property
-    def cam(self) -> cv2.VideoCapture:
-        return self._cam
+        return f"GigE (Port {self._src})"
 
     @property
     def initialized(self) -> bool:
         return self.cam.isOpened()
-
-    @property
-    def running(self) -> bool:
-        return self._running
     
-    @property
-    def incomplete_image_count(self) -> int:
-        return self._incomplete_image_count
     
     @property
     def real_fps(self) -> float:
@@ -288,16 +177,15 @@ class GigECamera:
     
     def start(self, continuous: bool = True) -> None:
         # Initialize the camera
-        if not self.initialized:
-            self.init()
+        self.init()
             
         # Begin acquisition
-        self._running = True
+        self.running = True
         
     def stop(self) -> None:
         self._frame_times = []
-        self._incomplete_image_count = 0
-        self._running = False
+        self.incomplete_image_count = 0
+        self.running = False
     
     def close(self) -> None:
         self.stop()
@@ -309,7 +197,7 @@ class GigECamera:
         
         # Increment incomplete image count if full image is not retrieved
         if not is_complete:
-            self._incomplete_image_count += 1
+            self.incomplete_image_count += 1
             
         # Ensure complete image is returned if option is chosen
         if complete_frames_only and not is_complete:
@@ -326,40 +214,10 @@ class GigECamera:
     def enable_auto_exposure(self) -> None:
         self.CAP_PROP_EXPOSURE = 0.75
         
-    def edit_settings(self) -> None:
-        """ 
-        Launch the platform-controlled webcam settings.
-        This is only available with the cv2.CAP_DSHOW backend.
-        """
-        self.CAP_PROP_SETTINGS = 0
-        
     def get_info(self, name: str) -> dict:
         info = {"name": name}
-        
-        
         return info
 
 
 if __name__ == "__main__":
-    
-    def test():
-        from PIL import Image 
-        
-        with UsbCamera() as cam:
-            while True:
-                try:
-                    
-                    for prop in cam._cap_props:
-                        print(prop, (getattr(cam, prop)))
-                        
-                    break
-                    
-                    array = cam.get_array()
-                    Image.fromarray(array).show()
-                    break
-                    
-                except KeyboardInterrupt:
-                    break
-        
-    test()
-        
+    pass
